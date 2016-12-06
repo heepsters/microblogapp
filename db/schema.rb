9999c40698d521249e7cf3b7565f6f8c4251b75a
Carson Hamilton <cjcham77@gmail.com>
2016-12-06 12:11:27 -0700

    fixed all the login bugs and set up the new tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161205224936) do
+ActiveRecord::Schema.define(version: 20161206185748) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string  "content"
+    t.integer "user_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string  "likes"
+    t.string  "dislikes"
+    t.string  "languages"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "fname"
